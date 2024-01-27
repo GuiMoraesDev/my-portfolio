8d6e19e1f8085d336e161d80437c1a3219b043ee
Guilherme Moraes <guimoraes.dev@gmail.com>
2024-01-27 16:01:38 -0300

    feat: add animated github icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import { Hamburger } from "@/assets/icons";
+import { Github, Hamburger } from "@/assets/icons";
 
 export default function Home() {
   return (
     <main className="flex h-screen w-screen flex-col items-center justify-center p-24">
       <Hamburger />
+      <Github />
     </main>
   );
 }

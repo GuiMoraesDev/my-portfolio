0340b588216130bf9feb8c8a087e66725287cdf6
Guilherme Moraes <guimoraes.dev@gmail.com>
2024-02-01 12:51:37 -0300

    refactor: refactor i18n settings

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,11 @@
 import createMiddleware from "next-intl/middleware";
+import { locales, defaultLocale } from "./i18n/settings";
 
 export default createMiddleware({
-  // A list of all locales that are supported
-  locales: ["en", "pt"],
-
-  // Used when no locale matches
-  defaultLocale: "en",
+  locales,
+  defaultLocale,
 });
 
 export const config = {
-  // Match only internationalized pathnames
-  matcher: ["/", "/(pt|en)/:path*"],
+  matcher: ["/", "/(en|pt)/:path*"],
 };

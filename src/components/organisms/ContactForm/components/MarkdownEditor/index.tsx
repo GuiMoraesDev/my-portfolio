1a48aa33b48014f9f56cf8a924f57cf581abf09c
Guilherme Moraes <guimoraes.dev@gmail.com>
2024-02-23 17:38:23 -0300

    feat: expand size of tiptap editor

--- src/components/organisms/ContactForm/components/MarkdownEditor/index.tsx
+++ src/components/organisms/ContactForm/components/MarkdownEditor/index.tsx
@@ -3,12 +3,14 @@
 import UnderlineMark from "@tiptap/extension-underline";
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import { useId } from "react";
 
 import { type FormProps } from "../../@types";
 
 import { Toolbar } from "./Toolbar";
 
 export const MarkdownEditor = (formMethods: FormProps) => {
+  const editorId = useId();
   const { register, setValue } = formMethods;
 
   const editor = useEditor({
@@ -23,21 +25,20 @@ export const MarkdownEditor = (formMethods: FormProps) => {
   }
 
   return (
-    <label className="flex w-full flex-col gap-1 divide-y rounded-md border border-gray-300 px-2">
+    <label
+      htmlFor={editorId}
+      className="flex w-full flex-col gap-1 divide-y rounded-md border border-gray-300 p-2"
+    >
       <EditorContent
         editor={editor}
         placeholder="Type your message here..."
-        className="h-full min-h-16 w-full resize p-2 [&_.tiptap]:h-full [&_.tiptap]:outline-0"
+        id={editorId}
+        className="flex h-full min-h-16 w-full cursor-auto resize [&_.tiptap]:h-auto [&_.tiptap]:w-full [&_.tiptap]:outline-0"
       />
 
       <Toolbar editor={editor} {...formMethods} />
 
-      <input
-        type="hidden"
-        id="message"
-        /* placeholder={t("form.placeholders.message")} */
-        {...register("message")}
-      />
+      <input type="hidden" id="message" {...register("message")} />
     </label>
   );
 };

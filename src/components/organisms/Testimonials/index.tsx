bc12ef529aeeba8d1a4e2bd3d2826e27532d6aa8
Guilherme Moraes <guimoraes.dev@gmail.com>
2024-02-24 12:13:46 -0300

    feat: add multiple testimonials avatars

--- src/components/organisms/Testimonials/index.tsx
+++ src/components/organisms/Testimonials/index.tsx
@@ -7,16 +7,28 @@ export const Testimonials = () => {
     <>
       <TestimonialCard
         name="Saulo Bulhões"
+        img={{
+          src: "/testimonial/avatar-1.png",
+          alt: "",
+        }}
         role={t("testimonials.saulo-bulhoes.role")}
         content={t("testimonials.saulo-bulhoes.text")}
       />
       <TestimonialCard
         name="Osmane Fonseca"
+        img={{
+          src: "/testimonial/avatar-2.png",
+          alt: "",
+        }}
         role={t("testimonials.osmane-fonseca.role")}
         content={t("testimonials.osmane-fonseca.text")}
       />
       <TestimonialCard
         name="Vitor Leonardo"
+        img={{
+          src: "/testimonial/avatar-3.png",
+          alt: "",
+        }}
         role={t("testimonials.vitor-leonardo.role")}
         content={t("testimonials.vitor-leonardo.text")}
       />
@@ -26,19 +38,28 @@ export const Testimonials = () => {
 
 type TestimonialCardProps = {
   name: string;
+  img: {
+    src: string;
+    alt: string;
+  };
   role: string;
   content: string;
 };
 
-const TestimonialCard = ({ name, role, content }: TestimonialCardProps) => (
+const TestimonialCard = ({
+  name,
+  img,
+  role,
+  content,
+}: TestimonialCardProps) => (
   <div className="mt-8 flex h-auto w-full max-w-xs border-collapse flex-col items-center justify-between rounded-md bg-plum-500/90 px-3 text-white backdrop-blur-sm md:max-w-[45%] md:justify-between lg:h-full lg:max-w-xs lg:flex-1">
     <Image
-      src="/testimonial-placeholder.png"
+      src={img.src}
       width={75}
       height={75}
       className="aspect-square h-auto -translate-y-1/2 select-none object-contain"
       priority
-      alt=""
+      alt={img.alt}
     />
 
     <p className="inline-flex h-max w-full items-start justify-start px-2 leading-tight tracking-wide  lg:h-full">

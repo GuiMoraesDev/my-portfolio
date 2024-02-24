35e94b3988a0a526a2b4247d5aab57b8639427d7
Guilherme Moraes <guimoraes.dev@gmail.com>
2024-02-24 08:09:30 -0300

    feat: add an placeholder to the editor

--- src/components/organisms/ContactForm/components/MarkdownEditor/index.tsx
+++ src/components/organisms/ContactForm/components/MarkdownEditor/index.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import Placeholder from "@tiptap/extension-placeholder";
 import UnderlineMark from "@tiptap/extension-underline";
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import { useId } from "react";
+import { twMerge } from "tailwind-merge";
 
 import { type FormProps } from "../../@types";
 
@@ -14,7 +16,14 @@ export const MarkdownEditor = (formMethods: FormProps) => {
   const { register, setValue } = formMethods;
 
   const editor = useEditor({
-    extensions: [StarterKit, UnderlineMark],
+    extensions: [
+      StarterKit,
+      UnderlineMark,
+      Placeholder.configure({
+        placeholder: "Type your message here...",
+      }),
+    ],
+    content: "",
     onUpdate({ editor }) {
       setValue("message", editor.getHTML());
     },
@@ -33,7 +42,11 @@ export const MarkdownEditor = (formMethods: FormProps) => {
         editor={editor}
         placeholder="Type your message here..."
         id={editorId}
-        className="flex h-full min-h-16 w-full cursor-auto resize [&_.tiptap]:h-auto [&_.tiptap]:w-full [&_.tiptap]:outline-0"
+        className={twMerge(
+          "h-full min-h-16 w-full cursor-auto",
+          "[&_.tiptap]:relative [&_.tiptap]:h-auto [&_.tiptap]:w-full [&_.tiptap]:outline-0",
+          "[&_.is-empty]:before:pointer-events-none [&_.is-empty]:before:float-left [&_.is-empty]:before:h-0 [&_.is-empty]:before:text-gray-400 [&_.is-empty]:before:content-[attr(data-placeholder)]",
+        )}
       />
 
       <Toolbar editor={editor} {...formMethods} />

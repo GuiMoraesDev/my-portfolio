0f53f4b16e5287a3760e49388d16d19e2d8967c5
Guilherme Moraes <guimoraes.dev@gmail.com>
2024-02-24 11:19:47 -0300

    chore: remove unused id

--- src/components/organisms/ContactForm/components/MarkdownEditor/index.tsx
+++ src/components/organisms/ContactForm/components/MarkdownEditor/index.tsx
@@ -4,7 +4,6 @@ import Placeholder from "@tiptap/extension-placeholder";
 import UnderlineMark from "@tiptap/extension-underline";
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
-import { useId } from "react";
 import { twMerge } from "tailwind-merge";
 
 import { type FormProps } from "../../@types";
@@ -12,7 +11,6 @@ import { type FormProps } from "../../@types";
 import { Toolbar } from "./Toolbar";
 
 export const MarkdownEditor = (formMethods: FormProps) => {
-  const editorId = useId();
   const { register, setValue } = formMethods;
 
   const editor = useEditor({
@@ -23,7 +21,6 @@ export const MarkdownEditor = (formMethods: FormProps) => {
         placeholder: "Type your message here...",
       }),
     ],
-    content: "",
     onUpdate({ editor }) {
       setValue("message", editor.getHTML());
     },
@@ -34,16 +31,12 @@ export const MarkdownEditor = (formMethods: FormProps) => {
   }
 
   return (
-    <label
-      htmlFor={editorId}
-      className="flex w-full flex-col gap-1 divide-y rounded-md border border-gray-300 p-2 pb-0"
-    >
+    <div className="flex w-full flex-col gap-1 divide-y rounded-md border border-gray-300 p-2 pb-0">
       <EditorContent
         editor={editor}
         placeholder="Type your message here..."
-        id={editorId}
         className={twMerge(
-          "h-full min-h-16 w-full cursor-auto",
+          "flex h-full min-h-16 w-full cursor-auto",
           "[&_.tiptap]:relative [&_.tiptap]:h-auto [&_.tiptap]:w-full [&_.tiptap]:outline-0",
           "[&_.is-empty]:before:pointer-events-none [&_.is-empty]:before:float-left [&_.is-empty]:before:h-0 [&_.is-empty]:before:text-gray-400 [&_.is-empty]:before:content-[attr(data-placeholder)]",
         )}
@@ -52,6 +45,6 @@ export const MarkdownEditor = (formMethods: FormProps) => {
       <Toolbar editor={editor} {...formMethods} />
 
       <input type="hidden" id="message" {...register("message")} />
-    </label>
+    </div>
   );
 };

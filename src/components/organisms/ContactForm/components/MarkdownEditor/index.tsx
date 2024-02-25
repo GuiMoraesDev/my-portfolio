c49a7d2ff0406d10f1182b5a7cd356b425abd2eb
Guilherme Moraes <guimoraes.dev@gmail.com>
2024-02-25 12:58:58 -0300

    style: fix editor padding

--- src/components/organisms/ContactForm/components/MarkdownEditor/index.tsx
+++ src/components/organisms/ContactForm/components/MarkdownEditor/index.tsx
@@ -16,7 +16,7 @@ export const MarkdownEditor = ({
   const { register } = formMethods;
 
   return (
-    <div className="flex h-32 w-full flex-col gap-1 divide-y rounded-md border border-gray-300 p-2 pb-0">
+    <div className="flex h-32 w-full flex-col gap-1 divide-y rounded-md border border-gray-300 px-3 pt-2">
       <EditorContent
         editor={editor}
         placeholder="Type your message here..."

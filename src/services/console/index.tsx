3c494227e7724787059dd45880c51a31aadef032
Guilherme Moraes <guimoraes.dev@gmail.com>
2024-02-29 10:05:48 -0300

    feat: update console.log message

--- src/services/console/index.tsx
+++ src/services/console/index.tsx
@@ -6,15 +6,15 @@ export const printInConsole = () => {
     console.clear();
   }
 
-  console.log(`  %c__________________ `, "color: #F2E2EC; font-size: 1.5em");
-  console.log(` %c|                 | `, "color: #F2E2EC; font-size: 1.5em");
-  console.log(` %c| Ah ah ah        | `, "color: #F2E2EC; font-size: 1.5em");
-  console.log(` %c| you didn't say  | `, "color: #F2E2EC; font-size: 1.5em");
-  console.log(` %c| the magic word! | `, "color: #F2E2EC; font-size: 1.5em");
-  console.log(` %c|_________________| `, "color: #F2E2EC; font-size: 1.5em");
-  console.log(`   %c      |      `, "color: #F2E2EC; font-size: 1.5em");
-  console.log(`   %c   O  |      `, "color: #F2E2EC; font-size: 1.5em");
-  console.log(`   %c  /|\\/       `, "color: #F2E2EC; font-size: 1.5em");
-  console.log(`   %c   |          `, "color: #F2E2EC; font-size: 1.5em");
-  console.log(`   %c  / \\        `, "color: #F2E2EC; font-size: 1.5em");
+  console.log(`%c   __________________  `, "color: #F2E2EC; font-size: 1.25em");
+  console.log(`%c  |                 |  `, "color: #F2E2EC; font-size: 1.25em");
+  console.log(`%c  | Ah ah ah        |  `, "color: #F2E2EC; font-size: 1.25em");
+  console.log(`%c  | you didn't say  |  `, "color: #F2E2EC; font-size: 1.25em");
+  console.log(`%c  | the magic word! |  `, "color: #F2E2EC; font-size: 1.25em");
+  console.log(`%c  |_________________|  `, "color: #F2E2EC; font-size: 1.25em");
+  console.log(`%c          |            `, "color: #F2E2EC; font-size: 1.25em");
+  console.log(`%c       O  |            `, "color: #F2E2EC; font-size: 1.25em");
+  console.log(`%c      /|\\/            `, "color: #F2E2EC; font-size: 1.25em");
+  console.log(`%c       |               `, "color: #F2E2EC; font-size: 1.25em");
+  console.log(`%c      / \\          🦖 `, "color: #F2E2EC; font-size: 1.25em");
 };

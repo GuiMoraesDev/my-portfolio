36602d588e089f46887dfbf6687666d35211e3c9
Guilherme Moraes <guimoraes.dev@gmail.com>
2024-03-19 16:21:26 -0300

    feat: add repos animation

--- src/components/organisms/Repos/Repos.tsx
+++ src/components/organisms/Repos/Repos.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { GitHubLogoIcon, Link2Icon } from "@radix-ui/react-icons";
+import { motion } from "framer-motion";
 import Image, { type ImageProps } from "next/image";
 import { useTranslations } from "next-intl";
 import { useState } from "react";
@@ -130,84 +131,106 @@ export const ReposComponent = () => {
           },
           index,
         ) => (
-          <li
+          <motion.li
             key={title}
-            className="group flex w-full flex-col gap-3 overflow-hidden rounded-lg bg-plum-50 p-4 text-plum-500 shadow-md md:flex-row"
+            initial="offscreen"
+            whileInView="onscreen"
+            viewport={{ once: true }}
+            className="group flex w-full"
           >
-            <div
-              className="relative block aspect-video h-full w-auto min-w-[45%] overflow-clip rounded-md transition"
-              style={{
-                order: index % 2 === 0 ? 0 : 1,
+            <motion.div
+              className="flex w-full flex-col gap-3 overflow-hidden rounded-lg bg-plum-50 p-4 text-plum-500 shadow-md md:flex-row"
+              variants={{
+                offscreen: {
+                  x: index % 2 === 0 ? 100 : -100,
+                  opacity: 0,
+                },
+                onscreen: {
+                  x: 0,
+                  opacity: 1,
+                  transition: {
+                    type: "spring",
+                    bounce: 0.4,
+                    duration: 0.8 * (index + 1),
+                  },
+                },
               }}
             >
-              <ImageComponent
-                src={cover}
-                fill
-                className="select-none object-cover drop-shadow-[0px_0px_4px_rgba(242,226,236,0.2)]"
-                alt={title}
-              />
-            </div>
+              <div
+                className="relative block aspect-video h-full w-auto min-w-[45%] overflow-clip rounded-md transition"
+                style={{
+                  order: index % 2 === 0 ? 0 : 1,
+                }}
+              >
+                <ImageComponent
+                  src={cover}
+                  fill
+                  className="select-none object-cover drop-shadow-[0px_0px_4px_rgba(242,226,236,0.2)]"
+                  alt={title}
+                />
+              </div>
 
-            <section
-              className="flex w-full flex-col gap-4"
-              style={{
-                order: index % 2 === 0 ? 1 : 0,
-              }}
-            >
-              <h3 className="text-2xl font-bold text-plum-700">{title}</h3>
+              <section
+                className="flex w-full flex-col gap-4"
+                style={{
+                  order: index % 2 === 0 ? 1 : 0,
+                }}
+              >
+                <h3 className="text-2xl font-bold text-plum-700">{title}</h3>
 
-              <p className="h-full overflow-hidden text-ellipsis">
-                {t("cards-description." + description_key)}
-              </p>
+                <p className="h-full overflow-hidden text-ellipsis">
+                  {t("cards-description." + description_key)}
+                </p>
 
-              <ul className="flex flex-wrap gap-3">
-                {tech_stack.map(({ name, color, link }) => (
-                  <li
-                    key={name}
-                    className="flex items-center gap-1 rounded-lg bg-white p-1.5 text-sm"
-                  >
+                <ul className="flex flex-wrap gap-3">
+                  {tech_stack.map(({ name, color, link }) => (
+                    <li
+                      key={name}
+                      className="flex items-center gap-1 rounded-lg bg-white p-1.5 text-sm"
+                    >
+                      <a
+                        href={link}
+                        className="inline-flex items-center gap-1"
+                        target="_blank"
+                      >
+                        <span
+                          className="inline-block h-2 w-2 rounded-full"
+                          style={{ backgroundColor: color }}
+                        />
+                        <span>{name}</span>
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+
+                <nav className="flex w-fit items-center gap-2 ">
+                  {project_link && (
                     <a
-                      href={link}
-                      className="inline-flex items-center gap-1"
+                      className="inline-flex gap-2 rounded-md bg-plum-500 p-2 text-white transition-colors hover:bg-plum-700"
+                      href={project_link}
+                      aria-label={`${title} project link: ${project_link}`}
+                      title={`${title} project link: ${project_link}`}
                       target="_blank"
                     >
-                      <span
-                        className="inline-block h-2 w-2 rounded-full"
-                        style={{ backgroundColor: color }}
-                      />
-                      <span>{name}</span>
+                      <Link2Icon className="h-6 w-6 " />
+                      Project
                     </a>
-                  </li>
-                ))}
-              </ul>
+                  )}
 
-              <nav className="flex w-fit items-center gap-2 ">
-                {project_link && (
                   <a
                     className="inline-flex gap-2 rounded-md bg-plum-500 p-2 text-white transition-colors hover:bg-plum-700"
-                    href={project_link}
-                    aria-label={`${title} project link: ${project_link}`}
-                    title={`${title} project link: ${project_link}`}
+                    href={github_link}
+                    aria-label={`${title} github link: ${project_link}`}
+                    title={`${title} github link: ${project_link}`}
                     target="_blank"
                   >
-                    <Link2Icon className="h-6 w-6 " />
-                    Project
+                    <GitHubLogoIcon className="h-6 w-6 " />
+                    Github
                   </a>
-                )}
-
-                <a
-                  className="inline-flex gap-2 rounded-md bg-plum-500 p-2 text-white transition-colors hover:bg-plum-700"
-                  href={github_link}
-                  aria-label={`${title} github link: ${project_link}`}
-                  title={`${title} github link: ${project_link}`}
-                  target="_blank"
-                >
-                  <GitHubLogoIcon className="h-6 w-6 " />
-                  Github
-                </a>
-              </nav>
-            </section>
-          </li>
+                </nav>
+              </section>
+            </motion.div>
+          </motion.li>
         ),
       )}
     </ul>

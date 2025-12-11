708e1f5f771a916148ec1474963c736faefa9ebe
Guilherme Moraes <guimoraes.dev@gmail.com>
2025-12-11 14:49:17 -0300

    feat: improve locale handling by retrieving locale from cookies

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,11 +2,17 @@ import { notFound } from "next/navigation";
 import { getRequestConfig } from "next-intl/server";
 
 import { locales } from "./settings";
+import { cookies } from "next/headers";
 
-export default getRequestConfig(async ({ locale }) => {
-  if (!locales.includes(locale as any)) notFound();
+export default getRequestConfig(async () => {
+  const store = await cookies();
+  const locale = store.get('locale')?.value || 'en';
+
+  if (!locales.includes(locale as any)) {
+    return notFound();
+  };
 
   return {
-    messages: (await import(`./locales/${locale}.json`)).default,
+    locale: (await import(`./locales/${locale}.json`)).default,
   };
 });

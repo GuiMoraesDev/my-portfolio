14b75a2533c7e7b573656866d368e693359b6e3d
Guilherme Moraes <guimoraes.dev@gmail.com>
2025-12-18 13:40:25 -0300

    feat: remove Robot and RobotAssistant components to streamline codebase

--- src/components/organisms/Robot/src/components/Backdrop.tsx
+++
@@ -1,1 +0,0 @@
-export const Backdrop = () => <circle cx="60" cy="60" r="56" fill="#E1C8B5" />;
--- src/components/organisms/Robot/src/components/Feet.tsx
+++
@@ -1,6 +0,0 @@
-export const Feet = () => (
-  <>
-    <rect x="32" y="104" width="22" height="10" rx="5" fill="#04363D" />
-    <rect x="66" y="104" width="22" height="10" rx="5" fill="#04363D" />
-  </>
-);
